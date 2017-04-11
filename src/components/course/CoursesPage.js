7e42566843d9940853d56dcadd24b6cee104c024
Senthil Ramalingam <racksen@gmail.com>
2017-04-11 09:04:23 -0700

    ch-10 and 11 changes

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,34 +1,15 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-
+import CourseList from './CourseList';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
 
   constructor(props,context){
     super(props,context);
-    
-    this.state = {
-      course: {title: ""}
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-    
   }
 
-  onTitleChange(event){
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave(event){
-    event.preventDefault();
-    //this.props.dispatch(courseActions.createCourse(this.state.course));
-    this.props.actions.createCourse(this.state.course);
-  }
 
   courseRow(course, index){
     return <div key={index}>{course.title}</div>;
@@ -38,17 +19,8 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
+        <CourseList courses={this.props.courses} />
         
-        <input type="text"
-          onChange={this.onTitleChange}
-          value = {this.state.course.title}/>
-        
-        <input type="submit"
-          value= "Save"
-          onClick={this.onClickSave} />
-      
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import { Provider } from 'react-redux';
 
 import routes from './routes';
 import configureStore from './store/configureStore';
+import {loadCourses} from './actions/courseActions';
 
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
